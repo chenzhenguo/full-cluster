c1489de5747e2e059f2fa0dd9e48b8d58d99c565
松鼠 <1426763507@qq.com>
2018-06-13 14:06:07 +0800

    spring-mvc+spring-security整合

---
+++ spring-mvc-server-four/src/main/java/com/hhcf/system/other/CustomSuccessHandler.java
@@ -0,0 +1,100 @@
+package com.hhcf.system.other;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import com.alibaba.fastjson.JSON;
+
+/**
+ * @Title: CustomSuccessHandler
+ * @Description:
+ * @Author: zhaotf
+ * @Since:2018年6月13日 下午1:53:04
+ * @see {@linkplain https://www.yiibai.com/spring-security/spring-security-4-hibernate-role-based-login-example.html}
+ */
+// @Component("customSuccessHandler")
+public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+	private Logger logger = LoggerFactory.getLogger(CustomSuccessHandler.class);
+	private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+
+	/**
+	 * 登录通过后，处理
+	 */
+	@Override
+	protected void handle(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
+			throws IOException {
+		String targetUrl = determineTargetUrl(authentication);
+
+		if (response.isCommitted()) {
+			System.out.println("Can't redirect");
+			return;
+		}
+
+		redirectStrategy.sendRedirect(request, response, targetUrl);
+	}
+
+	protected String determineTargetUrl(Authentication authentication) {
+		String url = "";
+		Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+		logger.info("determineTargetUrl:" + JSON.toJSONString(authorities));
+		List<String> roles = new ArrayList<String>();
+		for (GrantedAuthority a : authorities) {
+			roles.add(a.getAuthority());
+		}
+
+		if (isDba(roles)) {
+			url = "/db.do";
+		} else if (isAdmin(roles)) {
+			url = "/admin.do";
+		} else if (isUser(roles)) {
+			url = "/home.do";
+		} else {
+			url = "/accessDenied";
+		}
+		return url;
+	}
+
+	public void setRedirectStrategy(RedirectStrategy redirectStrategy) {
+		this.redirectStrategy = redirectStrategy;
+	}
+
+	protected RedirectStrategy getRedirectStrategy() {
+		return redirectStrategy;
+	}
+
+	private boolean isUser(List<String> roles) {
+		if (roles.contains("ROLE_USER")) {
+			return true;
+		}
+		return false;
+	}
+
+	private boolean isAdmin(List<String> roles) {
+		if (roles.contains("ROLE_ADMIN")) {
+			return true;
+		}
+		return false;
+	}
+
+	private boolean isDba(List<String> roles) {
+		if (roles.contains("ROLE_DBA")) {
+			return true;
+		}
+		return false;
+	}
+
+}
